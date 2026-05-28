85484da29e86ca883560cf013ff3608ec601adef
Karma1979 <57416448+ChenGolz@users.noreply.github.com>
2026-05-28 23:34:42 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="family-clock-v87-1-6-cloud-post-confirmfix";
+const CACHE_NAME="family-clock-v87-1-7-formpost-clean";
 const AUDIO_CACHE_NAME="family-clock-drive-audio-runtime-v3";
 
 const CORE_FILES=[
