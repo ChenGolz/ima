54e41fc0bbc47c3042a3b82727ee95fd3f26347a
Karma1979 <57416448+ChenGolz@users.noreply.github.com>
2026-05-30 12:39:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="family-clock-v87-1-15-music-dataurl-fix";
+const CACHE_NAME='family-clock-v87-1-17-incognito-cloudlink';
 const AUDIO_CACHE_NAME="family-clock-drive-audio-runtime-v3";
 
 const CORE_FILES=[
